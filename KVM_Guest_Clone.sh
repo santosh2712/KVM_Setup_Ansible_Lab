72cec79886940a528267ed47b47137761b48ada1
Santosh Kulkarni <santosh.kulkarni4u@gmail.com>
2020-07-10 22:29:04 +0530

    Ver:1.1 Added KVM Guest Support of 99 Guests

--- KVM_Guest_Clone.sh
+++ KVM_Guest_Clone.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #######################################################################################
 # Author    : Santosh Kulkarni System Administrator
-# Date      : 01-07-2020
+# Date      : 10-07-2020
 # Mail      : santosh.kulkarni4u@gmail.com
 # Phone     : +91-9960708564
-# Version   : 1.0
+# Version   : 1.1
 # Warrenty  : NO warrenty 
 #--------------------------------------------------------------------------------------
 # Purpose   : This script is for creating Ansible Lab of 4 Nodes having nameing 
@@ -19,16 +19,10 @@
 # Exit code 3 = libvirtd not Running oh Host
 # Exit code 4 = Host not supported 
 #==========================================#
-declare INTERNAL=internal
-declare NAT=default
+declare INTERNAL=my_lab_internal
+declare NAT=my_lab_nat
 #
-
-###### KVM Guest path Definition  ######
-# declare Base_Clone_KVM_NAME=${1:-base_vm}
-#### Network Definition ################
-# 
-#
-#==========================================#
+# =======================================#
 ######## Functions ###################
 DirCheckFunction () 
 {
@@ -43,6 +37,8 @@ function Print_lines_Funtion()
 	PrintCHAR=${1:-.}
 	printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' "$PrintCHAR"
 }
+# 
+
 # 
 HOST_OS_Version_Check_Function () 
 {
@@ -81,7 +77,7 @@ read -p "Enter kvm guest name to clone or use found kvm Default[$found_KVM]:" Ba
 # 
 Base_Clone_KVM_NAME=${Base_Clone_KVM_NAME:-"$found_KVM"}
 # 
-echo "Base_Clone_KVM_NAME :$Base_Clone_KVM_NAME"
+# echo "Base_Clone_KVM_NAME :$Base_Clone_KVM_NAME"
 }
 #
 Get_KVM_Guest_Installation_Path_Funtion ()
@@ -96,7 +92,7 @@ DirCheckFunction
 # 
 echo "KVM_Guest_Disk_DIR :$KVM_Guest_Disk_DIR"
 } 
-Script_Not_Compatible_with_Function ()
+Script_is_not_compatible_with_Host_OS_Function ()
 {
  Print_lines_Funtion "#"
  echo "OS System found  : $OS"
@@ -120,7 +116,7 @@ Fount_OS_Display_Funtion ()
 	echo "Found Host OS is $OS with Version : $VER"
 }
 
-ERROR_CODE1_ERROR_MASSSAGE_Function () 
+Error_Code_1_MASSAGE_Function () 
 {
 	Print_lines_Funtion =
 	echo "Exiting with Error CODE 1 . Possible reasons are as below"
@@ -133,6 +129,159 @@ ERROR_CODE1_ERROR_MASSSAGE_Function ()
 	Print_lines_Funtion =
 	exit 1 
 
+}
+# 
+Check_and_Define_Networks_IfnotFound_Funtion () 
+{
+#	
+Print_lines_Funtion =
+echo "Checking Virtual Networks $NAT $INTERNAL"
+Print_lines_Funtion =
+# 
+for i in "$NAT"  "$INTERNAL"  ; do
+	#
+	virsh net-info --network "$i"  > /dev/null 2>&1
+	if [[  $(echo $?) -eq  0  ]]; then
+		# 
+		Print_lines_Funtion -
+		echo "INFO:Network $i is already exist. Script will not recreate it"
+		Print_lines_Funtion -
+		# 
+	else 	
+		#
+		virsh net-destroy "$i" >> /dev/null 2>&1
+		virsh net-undefine  "$i" >> /dev/null 2>&1 
+		virsh net-define  --file "$i".xml >> /dev/null 2>&1 
+		virsh net-start   "$i" >> /dev/null 2>&1 
+		virsh net-autostart   "$i" >> /dev/null 2>&1 
+		# 
+		Print_lines_Funtion -
+		echo "INFO: Network $i is Created. Starting network $i"
+		Print_lines_Funtion -
+		# 
+		# 
+	fi
+	#
+done
+}
+# 
+Clear_all_Old_Cloned_Guests_Funtion () 
+{
+function YES ()
+ { 
+	for i in $(seq 1 99 ) ; do
+		# 
+		declare VMNAME="AnsNode_$i"
+		virsh shutdown --domain  "$VMNAME"   >> /dev/null 2>&1
+		wait
+		virsh destroy  --domain  "$VMNAME"  >> /dev/null 2>&1
+		wait 
+		virsh  undefine --domain "$VMNAME"  --delete-snapshots --remove-all-storage >> /dev/null 2>&1
+		wait 
+	done
+	Print_lines_Funtion -
+	echo "INFO: Cleared all KVM guest with from AnsNode_01 to AnsNode_99"	
+	Print_lines_Funtion -
+ }
+
+function  NO ()
+ {
+	Print_lines_Funtion -
+	echo "INFO: Script will only clear old instance for requested."
+	Print_lines_Funtion -
+
+ }
+ echo "Do you want to clear all existing guest with name Ansnode_* " 
+ echo -e  "\e[30;43;5;82m| Choose an option: Enter input between 1-3 only |\e[0m"\\n'==================================================='
+		options=("Clear All AnsNode_* Guests " "Clear only New AnsNode_ Guests" "Quit")
+        select opt in "${options[@]}"; do
+            #
+            case $REPLY in
+                1) YES ; break ;;
+                2) NO ; break ;;
+                3) break 2 ;;
+                *) echo "Wrong input! Please Input Numbers only from 1 to 3" >&2
+            esac
+            #
+        done
+
+}
+Create_Uniform_Guest_Clones_Funtion () 
+{
+# 
+# 
+Print_lines_Funtion -
+echo  "Script can clone Maximum 99 KVM guest with Fixed IP allocation"
+read -p "Enter Number of Clones you want for Lab Default [2]: " CloneCount
+Print_lines_Funtion -
+
+declare -i CloneCount=${CloneCount:-2}
+declare -i Last_VM_Clone_Number=$((CloneCount+0))
+
+if [[ $Last_VM_Clone_Number -lt 1 ]] ; then 
+	declare -i Last_VM_Clone_Number=1
+	echo "INFO: Incorrect input received Cloning 1 Guests. Input Value should be from 1 to 99 "
+elif [[ $Last_VM_Clone_Number -gt 99 ]]; then
+	declare -i Last_VM_Clone_Number=99
+	echo "INFO: Script can clone only 99 Guests.Input Value should be from 1 to 99 "
+fi
+echo "INFO: Script will Clone $Last_VM_Clone_Number KVM Guests. From $Base_Clone_KVM_NAME Guest"
+Print_lines_Funtion -
+
+for i in $(seq 1 $Last_VM_Clone_Number ) ; do
+	# 
+	# echo $i 
+	declare VMNAME="AnsNode_$i"
+	virsh shutdown --domain  "$VMNAME"   >> /dev/null 2>&1
+	wait
+	virsh destroy  --domain  "$VMNAME"  >> /dev/null 2>&1
+	wait
+	virsh  undefine --domain "$VMNAME"  --delete-snapshots --remove-all-storage >> /dev/null 2>&1
+	wait 
+	# 
+	declare Attached_Disk_SIZE=2G
+	declare KVM_DISK="$KVM_Guest_Disk_DIR/$VMNAME.qcow2"
+	
+	declare Attached_Disk="$Attached_Disks_DIR/$VMNAME"'_raw_'"$Attached_Disk_SIZE"
+	# 
+	rm -f "$KVM_DISK" >> /dev/null 2>&1
+	rm -f "$Attached_Disk" > /dev/null 2>&1
+	# 
+	# echo "Attached_Disk: $Attached_Disk"
+	Print_lines_Funtion =
+	echo "Cloning in Progress from $Base_Clone_KVM_NAME to $VMNAME"
+	Print_lines_Funtion 
+	# 
+	virt-clone  --check all=off  --original "$Base_Clone_KVM_NAME" --name "$VMNAME"  --file "$KVM_DISK" 
+	# 
+	qemu-img create -f  raw "$Attached_Disk" "$Attached_Disk_SIZE"     >> /dev/null 2>&1
+	echo "Additional disk $Attached_Disk is Created and it will be Attached to $VMNAME."
+	# 
+	virsh attach-disk --source "$Attached_Disk" --persistent --domain "$VMNAME"  --target 'vdb'
+	Print_lines_Funtion 
+	# 
+	echo "Attaching NIC eth$i TO $NAT network ON $VMNAME"
+	virsh attach-interface --domain "$VMNAME" --type network --source  "$NAT" --target "eth$i" --mac 52:54:00:10:10:"$i"  --current
+	Print_lines_Funtion
+	# 
+	echo "Attaching NIC ens$i TO $INTERNAL network ON $VMNAME"
+	virsh attach-interface --domain "$VMNAME" --type network --source "$INTERNAL"  --target "ens$i" --mac 52:54:00:20:20:"$i"  --current
+	Print_lines_Funtion 
+	echo "Setting Memory limit of 512 MB $VMNAME"
+	virsh  setmaxmem --domain "$VMNAME" --current --size 512m
+	wait
+	Print_lines_Funtion 
+	echo "Setting single CPU LIMIT for  $VMNAME"
+	virsh setvcpus --domain "$VMNAME" --current --count 1
+	wait
+	Print_lines_Funtion 
+
+	echo "Cloning done for $VMNAME ! Starting $VMNAME"
+	sleep 10  && virsh start  "$VMNAME" 
+	Print_lines_Funtion =
+	
+done
+# 
 }
 
 IF_HOST_OS_RedHat7_Or_CentOS7_Function () 
@@ -158,92 +307,26 @@ if [[ $EUID -eq 0 ]] && [ $VM_Exist_Count -eq 1 ] && [[ "$base_VM_State" = "$Bas
 	DirCheckFunction
 	# 
 	if [[ -w "$KVM_Guest_Disk_DIR"  ]] && [[ -w "$Attached_Disks_DIR" ]]  ; then
-		#statements
-		#	
-		Print_lines_Funtion =
-		echo "Refreshing Networks $NAT $INTERNAL"
-		Print_lines_Funtion =
-		# 
-		for i in "$NAT"  "$INTERNAL"  ; do
-			#
-			virsh net-info --network "$i"  > /dev/null 2>&1
-			if [[  $(echo $?) -eq  0  ]]; then
-				# 
-				Print_lines_Funtion -
-				echo "INFO:Network $i is already exist. Script not not recreate it"
-				Print_lines_Funtion -
-				# 
-			else 	
-				#
-				virsh net-destroy "$i" >> /dev/null 2>&1
-				virsh net-undefine  "$i" >> /dev/null 2>&1 
-				virsh net-define  --file "$i".xml >> /dev/null 2>&1 
-				virsh net-start   "$i" >> /dev/null 2>&1 
-				virsh net-autostart   "$i" >> /dev/null 2>&1 
-				# 
-				Print_lines_Funtion -
-				echo "Info: Network $i is Created. Starting network $i"
-				Print_lines_Funtion -
-				# 
-				virsh net-start "$i" >> /dev/null 2>&1
-				# 
-			fi
-			#
-		done
+			#statements
+			#	
+			Check_and_Define_Networks_IfnotFound_Funtion
 			#
 			declare Libvirtd_Status=$(systemctl is-active libvirtd)
 			# 
 		if [[ "$Libvirtd_Status" == "active" ]]; then
 			# 
 			Print_lines_Funtion -
-			echo "service libvirtd found running. Make Sure $Base_Clone_KVM_NAME is shutdown"
+			echo "INFO: Service libvirtd found running. "
 			Print_lines_Funtion -
 			# 
-			virsh shutdown --domain  base_vm  >> /dev/null 2>&1
 			# 
 			Print_lines_Funtion -
 			echo "Destroying & Recreating  KVM Guests with Name AnsNode1 to 4 "
 			Print_lines_Funtion -
-			#	 
-			for i in 1 2 3 4 ; do
-				# 
-				declare VMNAME="AnsNode_$i"
-				virsh shutdown --domain  "$VMNAME"   >> /dev/null 2>&1
-				virsh destroy  --domain  "$VMNAME"  >> /dev/null 2>&1
-				virsh undefine --domain  "$VMNAME"  >> /dev/null 2>&1
-				# 
-				declare KVM_DISK="$KVM_Guest_Disk_DIR/$VMNAME.qcow2"
-				declare Attached_Disk="$Attached_Disks_DIR/$VMNAME-2GB"
-				# 
-				rm -f "$KVM_DISK" >> /dev/null 2>&1
-				rm -f "$Attached_Disk" > /dev/null 2>&1
-				# 
-				Print_lines_Funtion =
-				echo "Cloning in Progress from $Base_Clone_KVM_NAME to $VMNAME"
-				Print_lines_Funtion =
-				# 
-				virt-clone  --check all=off  --original "$Base_Clone_KVM_NAME" --name "$VMNAME"  --file "$KVM_DISK" 
-				# 
-				qemu-img create -f  raw "$Attached_Disk" 2G     >> /dev/null 2>&1
-				echo "Creating $Attached_Disk to Attach $VMNAME "
-				Print_lines_Funtion =
-				# 
-				virsh attach-disk "$VMNAME"  --source  "$Attached_Disk" --target vdb --persistent
-				#
-				# echo "=========================================================="
-				# virsh autostart --domain  "$VMNAME"
-				sleep 10  && virsh start  "$VMNAME" 
-				# 
-				Print_lines_Funtion =
-				echo -e "Attaching NIC eth$i TO $NAT network.\\nAnd ens$i TO $INTERNAL network ON $VMNAME"
-				Print_lines_Funtion =
-				# 
-				virsh attach-interface "$VMNAME" network  "$NAT" 		--target "eth$i" --mac 52:54:00:35:10:5"$i"  --config --live
-				virsh attach-interface "$VMNAME" network  "$INTERNAL" 	--target "ens$i" --mac 52:54:00:34:98:5"$i"  --config --live
-				# virsh attach-interface --domain "$VMNAME"  --type bridge  --source br1 --model virtio --config --live
-				# 
-			done
-				# 
+			#
+			# 
+			Create_Uniform_Guest_Clones_Funtion 
+			# 
 		else
 			# 
 			Print_lines_Funtion =
@@ -261,7 +344,7 @@ if [[ $EUID -eq 0 ]] && [ $VM_Exist_Count -eq 1 ] && [[ "$base_VM_State" = "$Bas
 	# 
 else
 	# 
-	ERROR_CODE1_ERROR_MASSSAGE_Function
+	Error_Code_1_MASSAGE_Function
 	# 
 fi
 # 
@@ -278,7 +361,6 @@ Print_lines_Funtion
 # 
 Get_KVM_Guest_Installation_Path_Funtion
 # 
-# echo "Shutting down $Base_Clone_KVM_NAME.....!"
 virsh shutdown "$Base_Clone_KVM_NAME" > /dev/null 2>&1
 wait 
 # Print_lines_Funtion
@@ -291,95 +373,26 @@ if [[ $EUID -eq 0 ]] && [ $VM_Exist_Count -eq 1 ] && [[ "$base_VM_State" = "$Bas
 	DirCheckFunction
 	# 
 	if [[ -w "$KVM_Guest_Disk_DIR"  ]] && [[ -w "$Attached_Disks_DIR" ]]  ; then
-		#statements
-		#	
-		Print_lines_Funtion =
-		echo "Refreshing Networks $NAT $INTERNAL"
-		Print_lines_Funtion =
-		# 
-		for i in "$NAT"  "$INTERNAL"  ; do
-			#
-			virsh net-info --network "$i"  > /dev/null 2>&1
-			if [[  $(echo $?) -eq  0  ]]; then
-				# 
-				Print_lines_Funtion -
-				sudo virsh net-start --network "$i"  > /dev/null 2>&1
-				echo "INFO:Network $i is already exist. Script not not recreate it"
-				Print_lines_Funtion -
-				# 
-			else 	
-				#
-				virsh net-destroy "$i" >> /dev/null 2>&1
-				virsh net-undefine  "$i" >> /dev/null 2>&1 
-				virsh net-define  --file "$i".xml >> /dev/null 2>&1 
-				virsh net-start   "$i" >> /dev/null 2>&1 
-				virsh net-autostart   "$i" >> /dev/null 2>&1 
-				# 
-				Print_lines_Funtion -
-				echo "Info: Network $i is Created. Starting network $i"
-				Print_lines_Funtion -
-				# 
-				virsh net-start "$i" >> /dev/null 2>&1
-				# 
-			fi
-			#
-		done
+			#statements
+			#	
+			Check_and_Define_Networks_IfnotFound_Funtion
 			#
 			declare Libvirtd_Status=$(systemctl is-active libvirtd)
 			# 
 		if [[ "$Libvirtd_Status" == "active" ]]; then
 			# 
 			Print_lines_Funtion -
-			echo "service libvirtd found running. Make Sure $Base_Clone_KVM_NAME is shutdown"
+			echo "INFO: Service libvirtd found running. "
 			Print_lines_Funtion -
 			# 
-			virsh shutdown --domain  base_vm  >> /dev/null 2>&1
 			# 
 			Print_lines_Funtion -
-			echo "Destroying & Recreating  KVM Guests with Name AnsNode1 to 4 "
+			Clear_all_Old_Cloned_Guests_Funtion
 			Print_lines_Funtion -
-			#	 
-			for i in 1 2 3 4 ; do
-				# 
-				declare VMNAME="AnsNode_$i"
-				virsh shutdown --domain  "$VMNAME"   >> /dev/null 2>&1
-				virsh destroy  --domain  "$VMNAME"  >> /dev/null 2>&1
-				virsh undefine --domain  "$VMNAME"  >> /dev/null 2>&1
-				# 
-				declare KVM_DISK="$KVM_Guest_Disk_DIR/$VMNAME.qcow2"
-				declare Attached_Disk="$Attached_Disks_DIR/$VMNAME-2GB"
-				# 
-				rm -f "$KVM_DISK" >> /dev/null 2>&1
-				rm -f "$Attached_Disk" > /dev/null 2>&1
-				# 
-				Print_lines_Funtion =
-				echo "Cloning in Progress from $Base_Clone_KVM_NAME to $VMNAME"
-				Print_lines_Funtion =
-				# 
-				virt-clone  --check all=off  --original "$Base_Clone_KVM_NAME" --name "$VMNAME"  --file "$KVM_DISK" 
-				# 
-				qemu-img create -f  raw "$Attached_Disk" 2G     >> /dev/null 2>&1
-				echo "Creating Additional disk named $Attached_Disk to Attach $VMNAME "
-				# 
-				virsh attach-disk "$VMNAME"  --source  "$Attached_Disk" --target vdb --persistent
-				#
-				# echo "=========================================================="
-				# virsh autostart --domain  "$VMNAME"
-				Print_lines_Funtion =
-				Print_lines_Funtion 
-				echo -e "Attaching NIC eth$i TO $NAT network.\\nAnd ens$i TO $INTERNAL network ON $VMNAME"
-				virsh attach-interface --domain "$VMNAME" --type network --source  "$NAT" --target "eth$i" --mac 52:54:00:35:10:5"$i"  --current
-				Print_lines_Funtion 
-				virsh attach-interface --domain  "$VMNAME" --type network --source "$INTERNAL"  --target "ens$i" --mac 52:54:00:34:98:5"$i"  --current
-				echo "Cloning done for $VMNAME ! Starting $VMNAME"
-				Print_lines_Funtion =
-				# 
-				# 
-				sleep 10  && virsh start  "$VMNAME" 
-				# virsh attach-interface --domain "$VMNAME"  --type bridge  --source br1 --model virtio --config --live
-				# 
-			done
-				# 
+			#
+			# 
+			Create_Uniform_Guest_Clones_Funtion 
+			# 
 		else
 			# 
 			Print_lines_Funtion =
@@ -397,22 +410,22 @@ if [[ $EUID -eq 0 ]] && [ $VM_Exist_Count -eq 1 ] && [[ "$base_VM_State" = "$Bas
 	# 
 else 
 	# 
-	ERROR_CODE1_ERROR_MASSSAGE_Function
+	Error_Code_1_MASSAGE_Function
 	# 
 fi
 # 
 }
 
 #==========================================#
-###### Script Body #################
-
+#  Script Body Section 
+# 
 HOST_OS_Version_Check_Function
 # 
 # 
 if 	[[ $OS == "Red Hat" ]]  ; then
 	
 	if 	 [[ $OS == "Red Hat" ]] && [ $VER -eq 6 ]  ; then
-			Script_Not_Compatible_with_Function 
+			Script_is_not_compatible_with_Host_OS_Function 
 	elif [[ $OS == "Red Hat" ]] && [ $VER -eq 7 ]  ; then
 			IF_HOST_OS_RedHat7_Or_CentOS7_Function
 	elif [[ $OS == "Red Hat" ]] && [ $VER -eq 8 ]  ; then
@@ -423,13 +436,13 @@ if 	[[ $OS == "Red Hat" ]]  ; then
 			echo "In some cases kvm guest auto hostname setting not work. Check attached default.xml "
 			Print_lines_Funtion 
 	else 	
-			Script_Not_Compatible_with_Function
+			Script_is_not_compatible_with_Host_OS_Function
 	fi
 
 elif [[ $OS == "Debian" ]] ; then
-		 Script_Not_Compatible_with_Function
+		 Script_is_not_compatible_with_Host_OS_Function
 elif [[ $OS == "SuSE"   ]]; then
-		 Script_Not_Compatible_with_Function
+		 Script_is_not_compatible_with_Host_OS_Function
 else 
-		 Script_Not_Compatible_with_Function
+		 Script_is_not_compatible_with_Host_OS_Function
 fi
